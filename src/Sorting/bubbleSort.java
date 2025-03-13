b13ece43bdc66d16d3213b01cf05e29a7c9605ef
Himanshu kumar <himu2486@gmail.com>
2025-03-13 12:10:28 +0530

    add the new question

--- src/Sorting/bubbleSort.java
+++ src/Sorting/bubbleSort.java
@@ -8,23 +8,25 @@ public class bubbleSort {
         bubble(arr);
         System.out.println(Arrays.toString(arr));
     }
-    static void bubble(int[] arr){
+
+    static void bubble(int[] arr) {
         boolean swapped;
-        //run the steps n-1 times
-        for(int i = 0; i < arr.length -1; i++){
+        // Run the steps n-1 times
+        for (int i = 0; i < arr.length - 1; i++) {
             swapped = false;
-            //for each step, max item will come at the last respective index
-            for(int j = 1; j < arr.length -1; j++) {
-                //swap if the item is smaller than the previous item
-                if(arr[j] < arr[j -1]){
-                    //swap
+            // For each step, the largest item will move to the correct position
+            for (int j = 1; j < arr.length - i; j++) {
+                // Swap if the current item is smaller than the previous item
+                if (arr[j] < arr[j - 1]) {
+                    // Swap
                     int temp = arr[j];
-                    arr[j] = arr[j-1];
-                    arr[j-1] = temp;
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
                     swapped = true;
                 }
             }
-            if(!swapped){
+            // If no swaps occurred, the array is already sorted
+            if (!swapped) {
                 break;
             }
         }
