fc65134cba5c2a1e4da33aaf55de6144de88029d
Himanshu kumar <himu2486@gmail.com>
2025-03-16 22:35:12 +0530

    add the questiom

--- src/CyclicSort/FindAllDuplicates.java
+++ src/CyclicSort/FindAllDuplicates.java
@@ -1,7 +1,37 @@
 package CyclicSort;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FindAllDuplicates {
     public static void main(String[] args) {
+        int[] arr = {2, 5, 6, 2, 3, 5, 8, 7, 1, 4};
+        System.out.println(findDuplicate(arr));
+    }
 
+    public static List<Integer> findDuplicate(int[] arr) {  // Change the return type to List<Integer>
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index + 1) {
+                ans.add(arr[index]);
+            }
+        }
+        return ans;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
     }
 }
---
+++ src/CyclicSort/FindDuplicates.java
@@ -0,0 +1,30 @@
+package CyclicSort;
+
+public class FindDuplicates {
+    public static void main(String[] args) {
+      int[] arr = {5, 3, 3, 4, 1, 2};
+        System.out.println(findDuplicates(arr));
+    }
+    public static int findDuplicates(int[] arr){
+        int i =0;
+        while(i < arr.length){
+
+            if(arr[i] != i +1){
+                int correct = arr[i] -1;
+                if(arr[i] != arr[correct]){
+                    swap(arr, i, correct);
+                }else{
+                    return arr[i];
+                }
+            }else{
+                i++;
+            }
+        }
+        return -1;
+    }
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
