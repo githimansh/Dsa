1f56e7a708cfd76ea40244533a30372a71e4dd19
Himanshu kumar <himu2486@gmail.com>
2025-03-21 23:54:10 +0530

    add

--- src/BinarySearch/SearchInMountain.java
+++ src/BinarySearch/SearchInMountain.java
@@ -2,7 +2,7 @@ package BinarySearch;
 
 public class SearchInMountain {
     public static void main(String[] args) {
-       int[] arr = {0, 1, 2, 3, 4, 3, 2, 1};
+       int[] arr = {0, 1, 2, 3, 4, 3, 2, 1, 4, 5,6, 5};
         System.out.println(peakIndexInMoutainArray(arr));
     }
     int search(int[] arr, int target) {
