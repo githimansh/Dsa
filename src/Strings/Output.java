4dc2bf86151ecbcf2a8233546d41de3f58d02a93
Himanshu kumar <himu2486@gmail.com>
2025-03-29 23:06:05 +0530

    add

--- src/Strings/Output.java
+++ src/Strings/Output.java
@@ -2,7 +2,7 @@ package Strings;
 
 public class Output {
     public static void main(String[] args) {
-        System.out.println(24);
-        System.out.println(89);
+        System.out.println(25);
+        System.out.println(35);
     }
 }
