28d2c281df2616df031325393ae93f2e846e2b36
Himanshu kumar <himu2486@gmail.com>
2025-03-30 23:46:45 +0530

    add

--- src/Strings/Output.java
+++ src/Strings/Output.java
@@ -4,5 +4,9 @@ public class Output {
     public static void main(String[] args) {
         System.out.println(25);
         System.out.println(35);
+        System.out.println(89);
+        System.out.println(30);
+        System.out.println(28);
+        System.out.println(11);
     }
 }
