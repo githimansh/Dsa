675dd08c1e9af5c5909932a9ca26588d0973c499
Himanshu kumar <himu2486@gmail.com>
2025-03-31 23:53:55 +0530

    add

---
+++ src/Strings/PrettyPrinting.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 453.12845f;
+//       System.out.printf("Formatted number is %.2f", a);
+
+        System.out.println(Math.PI);
+        System.out.printf("Pie: %.3f", Math.PI);
+    }
+}
