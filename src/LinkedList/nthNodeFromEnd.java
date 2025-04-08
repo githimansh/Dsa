1b6f3fd6cc2de55e7332c30da53eea2ac00c67cc
Himanshu kumar <himu2486@gmail.com>
2025-04-09 02:35:49 +0530

    add

--- src/LinkedList/nthNodeFromEnd.java
+++ src/LinkedList/nthNodeFromEnd.java
@@ -1,10 +1,39 @@
 package LinkedList;
 
 public class nthNodeFromEnd {
+    public static Node nthNode(Node head, int n){
+        int size = 0;
+        Node temp = head;
+        while(temp != null){
+            size++;
+            temp = temp.next;
+        }
+        int m = size - n +1;
+        // mth node from start
+        temp = head;
+        for(int i = 1; i <= m-1; i++){
+            temp = temp.next;
+        }
+        return temp;
+    }
+    //in one traversal
+    public static Node nthNode2(Node head, int n){
+        Node slow = head;
+        Node fast = head;
+        for(int i = 1; i<=n; i++){
+            fast = fast.next;
+        }
+        while(fast != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
     public static class Node{
         int data;
         Node next;
         Node(int data){
+
             this.data = data;
         }
     }
@@ -21,5 +50,7 @@ public class nthNodeFromEnd {
         c.next = d;
         d.next = e;
         e.next = f;
+        Node q = nthNode2(a, 2);
+        System.out.println(q.data);
     }
 }
