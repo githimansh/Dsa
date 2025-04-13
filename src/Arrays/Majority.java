e44809c1b254702f2724e39393ff754562c1d34b
Himanshu kumar <himu2486@gmail.com>
2025-04-13 23:59:37 +0530

    add

--- src/Arrays/Majority.java
+++ src/Arrays/Majority.java
@@ -1,30 +1,24 @@
 package Arrays;
 
 public class Majority {
-    class Solution {
-        // Function to find the majority elements in the array
-        public List<Integer> findMajority(int[] nums) {
-            // Your code goes here.
-            ArrayList<Integer> list = new ArrayList<>();
-            HashMap<Integer, Integer> map = new HashMap<>();
+    public static void main(String[] args) {
+   int[]nums = {3, 2, 3};
 
-            int n = nums.length;
-
-            for(int i = 0; i <n; i++){
-                if(map.containsKey(nums[i])){
-                    map.put(nums[i], map.get(nums[i]) +1);
-
-                }else{
-                    map.put(nums[i], 1);
-                }
-            }
-            for(int key : map.keySet()){
-                if(map.get(key) > n/3){
-                    list.add(key);
-                }
-            }
-            return list;
-        }
     }
+    public static int majorityElement(int[] nums){
+        int majority = nums[0];
+        int count = 1;
 
+        for(int i =1; i <nums.length; i++){
+            if(count == 0){
+                count++;
+                majority = nums[i];
+            }else if(majority == nums[i]){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return majority;
+    }
 }
