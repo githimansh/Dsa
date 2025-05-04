654aacd7168a2b90f63f411bbcebf235d722d4a4
Himanshu kumar <himu2486@gmail.com>
2025-05-04 23:32:18 +0530

    aadd

---
+++ src/linearSearch/sort.java
@@ -0,0 +1,13 @@
+package linearSearch;
+
+public class sort {
+    class Solution {
+        int countFreq(int[] arr, int target) {
+            int count = 0;
+            for(int i =0; i <arr.length; i++){
+                if(arr[i] == target) count++;
+            }
+            return count;
+        }
+    }
+}
